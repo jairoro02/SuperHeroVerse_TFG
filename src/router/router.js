3c06475cd59344dfcb6bf8db8b1b8753c02927d9
jairoromero <jromero.alumno@iesrafaelalberti.es>
2023-06-03 13:39:41 +0200

    error pinia

---
+++ src/router/router.js
@@ -0,0 +1,23 @@
+import Home from '../pages/Home.vue';
+import Heroes from '../pages/Heroes.vue';
+import Villains from '../pages/Villains.vue';
+import Profile from '../pages/Profile.vue'
+import NoPageFound from '../shared/NoPageFound.vue';
+import Register from '../pages/Register.vue';
+import { createRouter, createWebHistory} from 'vue-router'
+
+const routes = [
+    { path: '/', name: 'home', component: Home},
+    { path: '/heroes', name: 'heroes', component: Heroes},
+    { path: '/villains', name: 'villains', component: Villains},
+    { path: '/profile', name: 'profile', component: Profile},
+    { path: '/profile', name: 'register', component: Register},
+    { path: '/:pathMatch(.*)*', component: NoPageFound },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+export default router
---
+++ src/shared/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <router-link :to="{ name: 'home' }">Home</router-link>
+        <router-link :to="{ name: 'heroes' }">Heroes</router-link>
+        <router-link :to="{ name: 'villains' }">Villains</router-link>
+        <router-link v-if="userStore.loggedIn" :to="{ name: 'profile' }">Profile</router-link>
+        <router-link v-else :to="{ name: 'register' }">Register</router-link>
+    </div>
+</template>
+
+<script>
+import { useUserStore } from '../storage/userStore';
+
+export default {
+  setup() {
+    const userStore = useUserStore();
+
+    return {
+      userStore,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
